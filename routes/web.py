7cec27f2720f10dc22e76d2f1f9731c91ab77256
boelchan <boelchan@live.com>
2023-06-06 07:58:25 +0700

    create a blog

--- routes/web.py
+++ routes/web.py
@@ -1,3 +1,8 @@
 from masonite.routes import Route
 
-ROUTES = [Route.get("/", "WelcomeController@show")]
+ROUTES = [
+    Route.get("/", "WelcomeController@show"),
+    
+    # Blog Routes
+    Route.get("/blog", "BlogController@show"),
+]
